c2b02318b12355ce226fb3307bb15b8f9d563762
Natalie Fearnley <nfearnley@gmail.com>
2020-06-05 14:59:42 -0400

    Fixed swtiny to scrape properly

--- cuteness/sources/swtiny.py
+++ cuteness/sources/swtiny.py
@@ -4,39 +4,58 @@ import random
 from cuteness.lib.cutepics import cutepics, PicSource, download_file, SourceNotReadyException
 
 
+class ChannelScraper:
+    def __init__(self, bot, channelid):
+        self.bot = bot
+        self.channelid = channelid
+        self.image_urls = []
+        self.scrape_delay = 2       # How many seconds to wait between scrapes
+        self.scrape_count = 200     # How many messages to fetch per scrape
+        self.last_message = None    # The last message id scraped
+        bot.add_listener(self.on_first_ready)
+
+    async def on_first_ready(self):
+        self.channel = self.bot.get_channel(557317063644020787)
+        asyncio.create_task(self.loop())
+
+    async def loop(self):
+        """Scrape the channel for unspoilered attachments"""
+        running = True
+        while running:
+            try:
+                running = await self.scrape()
+            except Exception as e:
+                print(f"Error while scraping channel #{self.channel.name} ({self.channel.id}): {e}")
+                pass
+            await asyncio.sleep(self.scrape_delay)
+
+    async def scrape(self):
+        """Scrape the channel for unspoilered attachments"""
+        print(f"Scraping the next {self.scrape_count} messages from #{self.channel.name}")
+        messages = await self.channel.history(limit=self.scrape_count, before=self.last_message).flatten()
+        if not messages:
+            return False
+        self.last_message = messages[-1].id
+        attachments = [a for m in messages for a in m.attachments]
+        urls = [a.url for a in attachments if not a.is_spoiler()]
+        self.image_urls.extend(urls)
+        print(f"Found {len(attachments)} urls ({len(self.image_urls)} total) in #{self.channel.name}{len(self.image_urls)}")
+        return True
+
+
+# TODO: Setup persistent file cache for image_urls
 class SWTiny(PicSource):
     category = "tiny"
 
     def __init__(self, bot):
         self.bot = bot
-        self.image_urls = []
-        bot.add_listener(self.on_first_ready)
-
-    async def on_first_ready(self):
-        asyncio.create_task(self.scrape_channel())
+        self.scraper = ChannelScraper(bot, 557317063644020787)
 
     async def fetch(self):
-        if not self.image_urls:
+        if not self.scraper.image_urls:
             raise SourceNotReadyException("No image urls scraped yet")
-        image_url = random.choice(self.image_urls)
+        image_url = random.choice(self.scraper.image_urls)
         return await download_file(image_url)
-
-    async def scrape_channel(self):
-        """Scrape the channel for unspoilered attachments"""
-        channel = self.bot.get_channel(557317063644020787)
-        running = True
-        before = None
-        while running:
-            await asyncio.sleep(5)
-            print("Scraping the next 200 message from #webfinds-sfw")
-            next_before = None
-            async for m in channel.history(limit=200, before=before):
-                next_before = m.id
-                self.image_urls.extend(a.url for a in m.attachments if not a.is_spoiler())
-            print(f"{len(self.image_urls)} urls available from #webfinds-sfw")
-
-            if next_before is None or len(self.image_urls) >= 2000:
-                running = False
 
 
 def setup(bot):
